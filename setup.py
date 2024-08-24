842f88841caae237eac662bcba46c39ac75f0006
Ehsan U <lancerf562@gmail.com>
2024-08-24 08:40:12 +0500

    bump 0.0.4 --> 0.0.5

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="scrapy-nodriver",
-    version="0.0.6",
+    version="0.0.5",
     license="BSD",
     description="Nodriver integration for Scrapy",
     long_description=long_description,
